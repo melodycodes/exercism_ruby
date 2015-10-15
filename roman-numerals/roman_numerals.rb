9655055f6899390d16aef7feb278a1344d797589
Melody <melody.caron@gmail.com>
2015-10-14 20:05:51 -0500

    Lint fixes.

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,32 +1,28 @@
 class Integer
+  NUM_TO_ROMAN = {
+    1000 => 'M',
+    900 => 'CM',
+    500 => 'D',
+    400 => 'CD',
+    100 => 'C',
+    90  => 'XC',
+    50  => 'L',
+    40  => 'XL',
+    10  => 'X',
+    9   => 'IX',
+    5   => 'V',
+    4   => 'IV',
+    1   => 'I'
+  }
 
-  def to_roman
-
-    num_to_roman = {
-      1000 => "M",
-      900 => "CM",
-      500 => "D",
-      400 => "CD",
-      100 => "C",
-      90  => "XC",
-      50  => "L",
-      40  => "XL",
-      10  => "X",
-      9   => "IX",
-      5   => "V",
-      4   => "IV",
-      1   => "I"
-    }
-
-    str = ""
+  def to_roman(str = '')
     r = self
 
-    num_to_roman.each do |k,v|
-      q,r = r.divmod(k)
+    NUM_TO_ROMAN.each do |k, v|
+      q, r = r.divmod(k)
       str << (v * q)
     end
 
     str
   end
-
 end
