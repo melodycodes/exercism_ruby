29b93db9387877dcf6faa9b9915e7707f0954241
Melody <melody.caron@gmail.com>
2015-10-15 18:43:21 -0500

    Cleaned up nth prime. Initial version of leap problem.

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -1,28 +1,23 @@
 class Prime
-    @@all_primes = []
+  @@all_primes = [2]
 
   def self.nth(n)
-    raise ArgumentError.new if n.zero?
-    return @@all_primes[n - 1] unless @@all_primes[n - 1].nil?
-    curr = @@all_primes.size
-    (@@all_primes.size...n).each do |i|
-      # increment current prime
+    fail ArgumentError, 'Argument must be nonzero' if n == 0
+
+    n -= 1
+    curr = @@all_primes.last + 1
+
+    (@@all_primes.size..n).each do |i|
+      curr += 1 until vfy_prime?(curr)
+      @@all_primes[i] = curr
       curr += 1
-      # find next prime
-      until vfy_prime?(curr)
-        curr += 1
-      end
-      @@all_primes[i] = curr # store ith prime
     end
-    @@all_primes[n - 1]
+
+    @@all_primes[n]
   end
 
   def self.vfy_prime?(num)
-    return false if num <= 1
-
-    (2...num).each do |d|
-      return false if num % d == 0
-    end
+    (2...num).each { |d| return false if num % d == 0 }
     true
   end
 end
