47d350e7df61ce553ab393b30e56d1d6ad439aab
Melody <melody.caron@gmail.com>
2015-10-14 19:56:27 -0500

    Lint fixes mostly. Renamed hash for clarity.

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,18 +1,16 @@
 class Raindrops
   def self.convert(num)
-    res = translate(num)
-    res.size.zero? ? num.to_s : res
+    res = FACTOR_TO_NOISES.map do |f, str|
+      str if (num % f).zero?
+    end.join
+
+    res.empty? ? num.to_s : res
   end
 
-  private
-    FACTOR_DECODE = {
-      3 => "Pling",
-      5 => "Plang",
-      7 => "Plong"
-    }
-
-    def self.translate(num)
-      FACTOR_DECODE.map {|f, str| str if (num % f).zero? }.join
-    end
+  FACTOR_TO_NOISES = {
+    3 => 'Pling',
+    5 => 'Plang',
+    7 => 'Plong'
+  }
 
 end
