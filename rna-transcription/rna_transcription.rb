b5201cc82da6cfd4708e95e70cffcc0a34e1a3e9
Melody <melody.caron@gmail.com>
2015-10-14 19:49:47 -0500

    Lint fixes.

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,34 +1,23 @@
+# Class to convert a DNA strand to its RNA complement, and vice versa
 class Complement
-
   # Define constants
   VERSION = 2
 
   DNA_VALUES = 'GCTA'
   RNA_VALUES = 'CGAU'
 
-  # DNA to RNA complement
-  # 'G' => 'C',
-  # 'C' => 'G',
-  # 'T' => 'A',
-  # 'A' => 'U'
   def self.of_dna(dstrand)
-    transform(dstrand,DNA_VALUES,RNA_VALUES)
+    transform(dstrand, DNA_VALUES, RNA_VALUES)
   end
 
-  # RNA to DNA complement
-  # 'C' => 'G',
-  # 'G' => 'C',
-  # 'A' => 'T',
-  # 'U' => 'A'
   def self.of_rna(rstrand)
-    transform(rstrand,RNA_VALUES,DNA_VALUES)
+    transform(rstrand, RNA_VALUES, DNA_VALUES)
   end
 
-  private
-  
-    def self.transform (strand, from, to)
-      raise ArgumentError.new if strand =~ /[^#{from}]/
-      strand.tr(from,to)
+  def self.transform(strand, from, to)
+    if strand =~ /[^#{from}]/
+      raise ArgumentError.new "The strand must only contain #{from}"
     end
-
+    strand.tr(from, to)
+  end
 end
