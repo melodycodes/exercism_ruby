d13fbe40da06ea4e66a7ea062a677fd4b3c1c097
Melody <melody.caron@gmail.com>
2015-10-15 19:05:56 -0500

    Cleaned up code. Only need to check factors up to the square root of n to verify if n is prime.

---
+++ leap/leap.rb
@@ -0,0 +1,7 @@
+class Year
+  VERSION = 1
+
+  def self.leap?(y)
+    (y % 4 == 0) && ((y % 100 != 0) || (y % 400 == 0))
+  end
+end
--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -4,20 +4,18 @@ class Prime
   def self.nth(n)
     fail ArgumentError, 'Argument must be nonzero' if n == 0
 
-    n -= 1
     curr = @@all_primes.last + 1
 
-    (@@all_primes.size..n).each do |i|
-      curr += 1 until vfy_prime?(curr)
-      @@all_primes[i] = curr
+    while @@all_primes.count < n
+      @@all_primes << curr if vfy_prime?(curr)
       curr += 1
     end
 
-    @@all_primes[n]
+    @@all_primes[n - 1]
   end
 
   def self.vfy_prime?(num)
-    (2...num).each { |d| return false if num % d == 0 }
+    2.upto(Math.sqrt(num)).each { |d| return false if num % d == 0 }
     true
   end
 end
