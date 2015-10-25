64ae4ba7c1713086b15c4e7821c3babb6d2f150a
Melody <melody.caron@gmail.com>
2015-10-25 13:03:56 -0500

    Finished and cleaned up crypto square.

--- crypto-square/crypto_square.rb
+++ crypto-square/crypto_square.rb
@@ -2,18 +2,31 @@ class Crypto
   attr_reader :size, :normalize_plaintext
   def initialize(str)
     @text = str
-    @normalize_plaintext = @text.downcase.gsub(/[^0-9a-z]/, '')
+    @normalize_plaintext = @text.downcase.gsub(/\W/, '')
     @size = Math.sqrt(@normalize_plaintext.size).ceil
-    @rows = @normalize_plaintext.chars.each_slice(@size).to_a
   end
 
   def plaintext_segments
-    @rows.map(&:join)
+    rows.map(&:join)
   end
 
   def ciphertext
-    row_size = @rows[0].size
-    padded = @rows.map {|i| i += (i.size != row_size) ? [''] * (row_size - i.size) : []}
-    padded.transpose.map(&:join).join
+    pad_transpose_text.join
+  end
+
+  def normalize_ciphertext
+    pad_transpose_text.join(' ')
+  end
+
+  private
+  def rows
+    @normalize_plaintext.chars.each_slice(@size).to_a
+  end
+
+  def pad_transpose_text
+    rsize = rows[0].size
+    rows.map do |row|
+      row += (row.size != rsize) ? [''] * (rsize - row.size) : []
+    end.transpose.map(&:join)
   end
 end
