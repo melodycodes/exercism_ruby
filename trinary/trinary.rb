5fc8933eb8f7f0807a9a448e009d15a1deeae092
Melody <melody.caron@gmail.com>
2015-10-25 08:46:56 -0500

    Removed regex check. Use inject instead of explicitly calculating the sum.

--- trinary/trinary.rb
+++ trinary/trinary.rb
@@ -4,11 +4,8 @@ class Trinary
   end
 
   def to_decimal
-    return 0 if @tri =~ /[^0-9]/
-    dec = 0
-    @tri.chars.reverse.each_with_index do |n, i|
-      dec += n.to_i * 3**i
-    end
-    dec
+    @tri.chars.reverse.map.with_index do |n, i|
+      n.to_i * 3**i
+    end.inject(:+)
   end
 end
