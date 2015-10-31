679f69fdb838723c087fff4e6dd92f86e4bc7b76
Melody <melody.caron@gmail.com>
2015-10-31 12:08:05 -0500

    Cleaned up implementation. Added constants for DAY and HOUR.

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,30 +1,26 @@
 class Clock
   attr_reader :minutes
 
+  HOUR = 60
+  DAY = 24 * HOUR
+
   def initialize(min = 0)
-    @minutes = min
+    @minutes = min % DAY
   end
 
-  def self.at(hour, min = 0)
-    minutes = min + 60 * hour
+  def self.at(hours, mins = 0)
+    minutes = mins + HOUR * hours
     new(minutes)
   end
 
   def +(other)
-    @minutes += other
+    update_minutes(other)
     self
   end
 
   def -(other)
-    @minutes -= other
+    update_minutes(-1 * other)
     self
-  end
-
-  def convert_to_hour_min(min)
-    h, m = min.divmod(60)
-    h = h % 24 if h >= 24
-    h += 24 if h < 0
-    [h, m]
   end
 
   def ==(other)
@@ -32,7 +28,15 @@ class Clock
   end
 
   def to_s
-    hour, min = convert_to_hour_min(@minutes)
+    hour, min = @minutes.divmod(HOUR)
     "#{hour.to_s.rjust(2, '0')}:#{min.to_s.rjust(2, '0')}"
+  end
+
+  private
+
+  def update_minutes(more_minutes)
+    @minutes += more_minutes
+    @minutes = @minutes % DAY
+    @minutes += DAY if @minutes < 0
   end
 end
