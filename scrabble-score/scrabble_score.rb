6438c9a416010cd6d3ffdaf95121da20aca7d144
Melody <melody.caron@gmail.com>
2015-11-08 21:29:48 -0600

    Refactored the Hash creation.

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -9,30 +9,27 @@ class Scrabble
     10 => %W[Q Z]
   }
 
+  score_array = []
+  SCORE_TO_LETTER.each do |s, l|
+    scores = Array.new(l.size) { s }
+    score_array += l.zip(scores)
+  end
+
+  SCORE_HASH = score_array.to_h
+
   def initialize(w = '')
     @word = w
-    build_score_hash
   end
 
   def score
     return 0 if @word.nil?
-    @word.chars.inject(0) do |sum, letter|
-      score = @scoring_hash[letter.upcase]
-      sum += score.nil? ? 0 : score
+    @word.chars.inject(0) do |total_score, letter|
+      letter_score = SCORE_HASH[letter.upcase]
+      total_score += letter_score.nil? ? 0 : letter_score
     end
   end
 
   def self.score(word)
     new(word).score
-  end
-
-  def build_score_hash
-    scores = []
-    score_array = []
-    SCORE_TO_LETTER.each do |score, letters|
-      scores = Array.new(letters.size) { score }
-      score_array += letters.zip(scores)
-    end
-    @scoring_hash = score_array.to_h
   end
 end
