ecfc985a2af583898d28228aaca745b7b308a40e
Melody <melody.caron@gmail.com>
2016-09-06 15:34:27 -0700

    Clean up code for hexadecimal exercise

--- hexadecimal/hexadecimal.rb
+++ hexadecimal/hexadecimal.rb
@@ -1,33 +1,15 @@
 class Hexadecimal
-  HEX_DECODE = {
-    "0" => 0,
-    "1" => 1,
-    "2" => 2,
-    "3" => 3,
-    "4" => 4,
-    "5" => 5,
-    "6" => 6,
-    "7" => 7,
-    "8" => 8,
-    "9" => 9,
-    "A" => 10,
-    "B" => 11,
-    "C" => 12,
-    "D" => 13,
-    "E" => 14,
-    "F" => 15
-  }.freeze
+  HEX_DECODE = '0123456789abcdef'.chars.zip(0..15).to_h
 
-  def initialize(n)
-    @hex_str = n.upcase
-    @hex_chars = @hex_str.chars
+  def initialize(hex_string)
+    @hex_string = hex_string.downcase
   end
 
   def to_decimal
-    @hex_chars.reverse.each_with_index.inject(0) do |total, (hex_char, i)|
-      decimal_val = HEX_DECODE[hex_char]
-      return 0 unless decimal_val
-      total += decimal_val * (16 ** i)
-    end
+    @hex_string.chars.reverse.map.with_index do |hex_char, i|
+      HEX_DECODE[hex_char] * 16 ** i
+    end.reduce(&:+)
+  rescue
+    0
   end
 end
